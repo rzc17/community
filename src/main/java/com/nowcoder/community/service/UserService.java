b3b17ed75b72879c0e3aba60414abbbc1c99eb04
Ren Zechao <1012582061@qq.com>
2021-06-12 16:03:44 +0800

    3.8发送列表

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -10,11 +10,9 @@ import com.nowcoder.community.util.MailClient;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import org.w3c.dom.ls.LSOutput;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -48,42 +46,44 @@ public class UserService implements CommunityConstant {
 
     public Map<String, Object> register(User user) {
         Map<String, Object> map = new HashMap<>();
+
         // 空值处理
         if (user == null) {
-            throw new IllegalArgumentException("参数不能为空！");
+            throw new IllegalArgumentException("参数不能为空!");
         }
         if (StringUtils.isBlank(user.getUsername())) {
-            map.put("usernameMsg", "账号不能为空！");
+            map.put("usernameMsg", "账号不能为空!");
+            return map;
         }
         if (StringUtils.isBlank(user.getPassword())) {
-            map.put("passwordMsg", "密码不能为空！");
+            map.put("passwordMsg", "密码不能为空!");
             return map;
         }
         if (StringUtils.isBlank(user.getEmail())) {
-            map.put("emailMsg", "邮箱不能为空！");
+            map.put("emailMsg", "邮箱不能为空!");
             return map;
         }
 
         // 验证账号
         User u = userMapper.selectByName(user.getUsername());
         if (u != null) {
-            map.put("usernameMsg", "该账号已存在");
+            map.put("usernameMsg", "该账号已存在!");
             return map;
         }
 
         // 验证邮箱
         u = userMapper.selectByEmail(user.getEmail());
         if (u != null) {
-            map.put("emailMsg", "该邮箱已被注册");
+            map.put("emailMsg", "该邮箱已被注册!");
             return map;
         }
 
-        //注册用户
+        // 注册用户
         user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
         user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
-        user.setType(0); //普通用户
-        user.setStatus(0); //未激活
-        user.setActivationCode(CommunityUtil.generateUUID()); //激活码
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
         user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
         user.setCreateTime(new Date());
         userMapper.insertUser(user);
@@ -92,10 +92,11 @@ public class UserService implements CommunityConstant {
         Context context = new Context();
         context.setVariable("email", user.getEmail());
         // http://localhost:8080/community/activation/101/code
-        String url = domain + contextPath + "/activation/" + user.getId() +"/" + user.getActivationCode();
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
         context.setVariable("url", url);
-        String content = templateEngine.process("mail/activation", context);
+        String content = templateEngine.process("/mail/activation", context);
         mailClient.sendMail(user.getEmail(), "激活账号", content);
+
         return map;
     }
 
@@ -127,13 +128,14 @@ public class UserService implements CommunityConstant {
         // 验证账号
         User user = userMapper.selectByName(username);
         if (user == null) {
-            map.put("usernameMsg", "该账号不存在");
+            map.put("usernameMsg", "该账号不存在!");
             return map;
         }
 
         // 验证状态
         if (user.getStatus() == 0) {
-            map.put("usernameMsg","该账号未激活!");
+            map.put("usernameMsg", "该账号未激活!");
+            return map;
         }
 
         // 验证密码
@@ -159,12 +161,16 @@ public class UserService implements CommunityConstant {
         loginTicketMapper.updateStatus(ticket, 1);
     }
 
-    public LoginTicket  findLoginTicket(String ticket) {
+    public LoginTicket findLoginTicket(String ticket) {
         return loginTicketMapper.selectByTicket(ticket);
     }
 
     public int updateHeader(int userId, String headerUrl) {
         return userMapper.updateHeader(userId, headerUrl);
+    }
+
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
     }
 
 }
